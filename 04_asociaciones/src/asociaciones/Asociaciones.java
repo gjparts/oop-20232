ab24a69b01b86b6136a3927b5d65c031e7a407bb
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-06-14 09:51:05 -0600

    commit 140620236204

--- 04_asociaciones/src/asociaciones/Asociaciones.java
+++ 04_asociaciones/src/asociaciones/Asociaciones.java
@@ -5,16 +5,42 @@ public class Asociaciones {
     public static void main(String[] args) {
         // TODO code application logic here
         Operador tigo = new Operador("TIGO","Honduras");
+        Operador claro = new Operador("Claro C.A.","Honduras");
         
         Fabricante f1 = new Fabricante("SAMSUNG","Korea","Kim Algo");
-        
+        Fabricante f2 = new Fabricante("Sony","Japon","Satoru Iwata");
+        Fabricante f3 = new Fabricante("Apple","USA","Tim Cook");
+                
         Bateria b1 = new Bateria(f1,4500f);
+        Bateria b2 = new Bateria(f2,3500f);
+        Bateria b3 = new Bateria(f2,6000f);
+        Bateria b4 = new Bateria(f3,5500f);
+        
         //lo siguiente no deberia ser permitido
         //segun el diagrama tengo una relacion
         //por composicion, por lo tanto
         //el Fabricante debe ser un objeto
         //que no sea nulo.
         //Bateria b2 = new Bateria(null,4500f);
+        
+        Chip sim1 = new Chip(tigo, 99991515);
+        Chip sim2 = new Chip(tigo, 97971616);
+        Chip sim3 = new Chip(claro, 33331515);
+        Chip sim4 = new Chip(claro, 34345656);
+        
+        Smartphone s20 = new Smartphone(f1, "Galaxy S20", 256, 8, b1);
+        s20.chip1 = sim1;
+        System.out.println( s20.chip1 );
+        System.out.println( s20.chip2 );
+        s20.imprimir();
+        
+        Smartphone iphone14 = new Smartphone(f3, "iPhone 14", 512, 8, b4);
+        iphone14.imprimir();
+        
+        Smartphone sony1 = new Smartphone(f2, "Ericsson w50000", 256, 4, b2);
+        sony1.chip1 = sim2;
+        sony1.chip2 = sim3;
+        sony1.imprimir();
         
     }
     
--- 04_asociaciones/src/asociaciones/Smartphone.java
+++ 04_asociaciones/src/asociaciones/Smartphone.java
@@ -39,4 +39,70 @@ public class Smartphone {
         this.almacenamiento = almacenamiento;
         this.ram = ram;
     }
+    //metodos
+    /**
+     * Devuelve la marca de quien fabrica el smartphone
+     * @return un objeto de clase Fabricante
+     */
+    public Fabricante getMarca(){
+        return this.marca;
+    }
+    /**
+     * Devuelve el modelo del smartphone
+     * @return un String
+     */
+    public String getModelo(){
+        return this.modelo;
+    }
+    /**
+     * Devuelve la capacidad en Gigabytes para la memoria interna del smartphone
+     * @return un numero entero
+     */
+    public int getAlmacenamiento(){
+        return this.almacenamiento;
+    }
+    /**
+     * Devuelve la cantidad de RAM del smartphone en Gigabytes
+     * @return un numero entero
+     */
+    public int getRam(){
+        return this.ram;
+    }
+    /**
+     * Devuelve la bateria del smartphone
+     * @return un objeto de clase Bateria
+     */
+    public Bateria getBateria(){
+        return this.bateria;
+    }
+    
+    /**
+     * imprime una ficha tecnica del smartphone
+     */
+    public void imprimir(){
+        System.out.println("************** Smartphone **************");
+        System.out.println("Fabricante: "+this.marca.nombre);
+        System.out.println("Modelo: "+this.modelo);
+        System.out.println("Almacenamiento: "+this.almacenamiento+"Gb");
+        System.out.println("RAM: "+this.ram+"Gb");
+        System.out.println("Bateria:");
+        System.out.println("\tMarca: "+this.bateria.getMarca().nombre);
+        System.out.println("\tMiliAmperios: "+this.bateria.miliAmperios);
+        if( this.chip1 == null )
+            System.out.println("Chip 1: No tiene");
+        else
+        {
+            System.out.println("Chip 1:");
+            System.out.println("\tOperador: "+this.chip1.getProveedor().nombre);
+            System.out.println("\tNumero: "+this.chip1.numeroTelefonico);
+        }
+        if( this.chip2 == null )
+            System.out.println("Chip 2: No tiene");
+        else
+        {
+            System.out.println("Chip 2:");
+            System.out.println("\tOperador: "+this.chip2.getProveedor().nombre);
+            System.out.println("\tNumero: "+this.chip2.numeroTelefonico);
+        }
+    }
 }
